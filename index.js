8a2854ff4e0b451497e6a768c74bab085fa88163
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-06-15 21:45:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+let mondayWork = function(work="go to the office"){
+    return `This Monday, I will ${work}.`;
+};
+
+function wrapAdjective(parm2 = "*") {
+    return function(parm1 = "special") {
+      return `You are ${parm2}${parm1}${parm2}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  
+  
